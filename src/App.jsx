128f21d2d935b4bc9a0df44830cdbbb295b69c7b
CodingBoy16 <rs1644818@gmail.com>
2025-02-28 11:10:32 +0530

    Removed The Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes,Route } from "react-router-dom";
-import  Login  from "./pages/login";
+import  Login  from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Main from "./pages/main";
+import Employee from "./pages/Employee";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Main/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/signup" element={<SignUp/>}></Route>
+      <Route path="/emplyoee" element={<Employee/>}></Route>
     </Routes>
     </BrowserRouter>
     </>
