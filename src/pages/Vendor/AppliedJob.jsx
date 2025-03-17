015b542ad7d2b2498e247b03b793ed543c13603c
Shanya-Tripathi <shanyatripathi910@gmail.com>
2025-03-17 16:35:17 +0530

    Some Basic Changes

--- src/pages/Vendor/AppliedJob.jsx
+++ src/pages/Vendor/AppliedJob.jsx
@@ -16,10 +16,10 @@ const AppliedJob = () => {
       <h2 className="text-2xl font-bold mb-2 text-center kanit-medium py-3">--Applied Jobs--</h2>
       <div className="bg-white px-5 rounded ">
         <p className="text-gray-700 text-xl font-medium">List of jobs you have applied for will be displayed here.</p>
-        <div className="flex flex-wrap justify-between gap-4 p-4">
+        <div className="flex flex-wrap justify-around md:gap-4 md:p-4">
       {/* Section 1 */}
       {apiData.map((data) => (
-        <div key={data.id} className="card w-56 flex-shrink-0">
+        <div key={data.id} className="card w-56 flex">
           <img src={data.avatar} className="card-img-top" alt="Avatar" />
           <div className="card-body">
             <h5 className="card-title">
